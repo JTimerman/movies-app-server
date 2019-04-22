6c42645371245e236df1f3a99f6359b2ffaebbff
denbalestri <denisbalestri@hotmail.com>
2019-04-22 18:47:37 -0300

    Cree el controlador de comentarios

---
+++ controllers/ComentarioController.js
@@ -0,0 +1,47 @@
+var comentario = require('../models/Comentario');
+var bodyParser = require('body-parser');
+var peliculaController=require ('../controllers/peliculaController');
+    
+let getComentarios = (req, res) =>
+{      
+   // console.log("llegue a leer");
+    //Listar resultados
+    comentario.find()
+    .then
+    (
+        (listaComentarios)=>
+        {
+            res.send(listaComentarios); //devuelvo resultado query   
+            //console.log(listaContactos);    
+        },
+        (err)=>{console.log(err);}
+    )       
+};
+
+
+
+let setComentario = (req,res) =>
+{
+   // console.log(req.body);
+    var newComentario = Comentario({
+        comentario:req.body.comentario,
+        puntaje:req.body.puntaje,
+        nombreUsuario:req.body.nombreUsuario,
+        pelicula: req.body.pelicula,
+        
+    });
+    newComentario.save().
+    then
+    (
+        (newComentario)=>
+        {
+            //res.send(newVoto); //devuelvo resultado query 
+           // getVotosEquipo(req.body.nombre); 
+           peliculaController.updatePelicula(req.body.pelicula,req.body.puntaje);   
+        },
+        (err)=>{console.log(err);}
+    ) 
+}
+
+
+module.exports = {getComentarios,setComentario};
