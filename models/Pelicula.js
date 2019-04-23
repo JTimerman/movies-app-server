8d27ed875a2d36356ca6dcab25a8c19b07a28ae2
unknown <gustavo.g.negri@gmail.com>
2019-04-23 09:10:44 -0300

    update Peliculas

--- models/Pelicula.js
+++ models/Pelicula.js
@@ -1,15 +1,14 @@
 var mongoose = require('mongoose');
 
-var peliculaSchema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
 var peliculaSchema = new Schema({
     nombre:String,
     promedio:Number, 
-    cantidadVotos:Number    
+    cantidadVotos:Number,
+    votosTotales:Number    //lala
 });
 
 var Peliculas = mongoose.model('Pelicula', peliculaSchema);
 console.log("se creo modelo");
 module.exports = Peliculas;
-
-
