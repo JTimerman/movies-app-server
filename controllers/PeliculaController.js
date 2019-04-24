5dc4538cc62b098381272b0872051a851f22b4e0
denbalestri <denisbalestri@hotmail.com>
2019-04-24 18:32:02 -0300

    edite

--- controllers/PeliculaController.js
+++ controllers/PeliculaController.js
@@ -118,13 +118,13 @@ let setPelicula = (req,res) =>
    //console.log('entre a set')
    //creo nueva pelicula
    var newPelicula = peliculas({
-    nombre:req.body.nombre,
-    promedio:req.body.promedio,
-    cantidadVotos:req.body.cantidadVotos,
-    totalPuntaje:req.body.totalPuntaje,
-    idPelicula:req.body.idPelicula
+    nombre:req.body.pelicula.nombre,
+    promedio:req.body.pelicula.promedio,
+    cantidadVotos:req.body.pelicula.cantidadVotos,
+    totalPuntaje:req.body.pelicula.totalPuntaje,
+    idPelicula:req.body.pelicula.idPelicula
     });
-console.log(newPelicula);
+//console.log(newPelicula);
 //verico que no exista la peli
     //let busqueda = {nombre: req.body.nombre,promedio:req.body.promedio,cantidadVotos:req.body.cantidadVotos,totalPuntaje:req.body.totalPuntaje,idPelicula:req.body.idPelicula};
     let busqueda = {idPelicula:req.body.idPelicula};
