dcd081e2bd8cf3a0cb94504a04f3033feb5fe857
denbalestri <denisbalestri@hotmail.com>
2019-04-24 18:08:41 -0300

    edite

--- controllers/ComentarioController.js
+++ controllers/ComentarioController.js
@@ -46,7 +46,7 @@ let setComentario = (req,res) =>
          
             if(peliculaEncontrada.length==0){
                 
-                peliculaController.setPelicula(req.body.pelicula)
+                peliculaController.setPelicula(req)
                 .then
                 (
                     (BuscadadaPelicula)=>{
